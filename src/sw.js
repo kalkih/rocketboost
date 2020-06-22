295e4439548db34be2936f3d3d403ffd26853255
Karl Kihlström <karl.kihlstrom@softhouse.se>
2020-06-22 17:53:30 +0200

    :bug: Ensure notification is displayed on push

--- src/sw.js
+++ src/sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('push', (event) => {
+  event.waitUntil(handlePush(event))
+})
+
+const handlePush = async (event) => {
   const channel = new BroadcastChannel('sw-push')
   const message = JSON.parse(event.data.text())
   channel.postMessage(message)
 
-  event.waitUntil(showNotification(message))
-})
-
-const showNotification = (message) => {
-  const { title, body, timestamp } = message.data
+  const { title, body, timestamp } = message
   // Possibly utilize notification tag here
   const options = {
     body,
@@ -15,7 +15,7 @@ const showNotification = (message) => {
     badge: '/img/icons/android-chrome-any-192x192.png',
     timestamp,
   }
-  self.registration.showNotification(title || process.env.VUE_APP_NAME, options)
+  return self.registration.showNotification(title, options)
 }
 
 // workbox.clientsClaim()

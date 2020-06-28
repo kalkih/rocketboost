350314641341b2d58589f9a458ae2ad3f2626de6
Karl Kihlström <karl.kihlstrom@softhouse.se>
2020-06-28 13:20:45 +0200

    :speech_balloon: Update notification error text

--- src/lang/errors.js
+++ src/lang/errors.js
@@ -1,2 +1,2 @@
-export const NOTIFICATIONS_NOT_SUPPORTED = 'Push notifications are not supported by your browser'
+export const NOTIFICATIONS_NOT_SUPPORTED = 'Push notifications are not supported on your device'
 export const NOTIFICATION_PERMISSION_DENIED = `${process.env.VUE_APP_NAME} requires permission to send notifications for this feature, please allow ${process.env.VUE_APP_NAME} in settings`

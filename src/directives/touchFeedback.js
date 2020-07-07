b7d28aa82472c9721f5ba81b79e1b2b07400373e
Karl Kihlström <karl.kihlstrom@softhouse.se>
2020-07-07 20:26:21 +0200

    :sparkles: Emit events from touch-feedback directive

--- src/directives/touchFeedback.js
+++ src/directives/touchFeedback.js
@@ -1,23 +1,32 @@
-const hover = (event) => {
+const NAMESPACE = 'touch-feedback'
+
+function hover(event) {
+  this.dispatchEvent(new Event(`${NAMESPACE}-hover`))
   event.currentTarget.classList.add('--hover')
 }
-const down = (event) => {
+function down(event) {
+  this.dispatchEvent(new Event(`${NAMESPACE}-pressed`))
   event.currentTarget.classList.add('--hover')
   event.currentTarget.classList.add('--pressed')
 }
-const reset = (event) => {
+function reset(event) {
+  this.dispatchEvent(new Event(`${NAMESPACE}-reset`))
   event.currentTarget.classList.remove('--hover')
   event.currentTarget.classList.remove('--pressed')
 }
 
 export default {
-  bind(el) {
+  bind(el, _bindings, vnode) {
+    // eslint-disable-next-line no-param-reassign
+    el.$$touch = {
+      vnode,
+    }
     el.addEventListener('mousedown', down, { capture: true })
     el.addEventListener('mouseup', reset, { capture: true })
     el.addEventListener('touchstart', down, { capture: true, passive: true })
     el.addEventListener('touchend', reset, { capture: true, passive: true })
-    el.addEventListener('mouseleave', reset, { capture: true })
-    el.addEventListener('mouseover', hover, { capture: true })
+    el.addEventListener('mouseleave', reset)
+    el.addEventListener('mouseenter', hover)
   },
   unbind(el) {
     el.removeEventListener('mousedown', down)
@@ -25,6 +34,6 @@ export default {
     el.removeEventListener('touchstart', down)
     el.removeEventListener('touchend', reset)
     el.removeEventListener('mouseleave', reset)
-    el.removeEventListener('mouseover', hover)
+    el.removeEventListener('mouseenter', hover)
   },
 }

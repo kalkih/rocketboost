68a72319bb98d15fd24d66200963d3d84f91034a
Karl Kihlström <karl.kihlstrom@softhouse.se>
2020-07-08 21:46:37 +0200

    :sparkles: Open relevant page in app when pressing notification

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-restricted-globals */
+importScripts('./env-vars.js')
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest)
 workbox.routing.registerNavigationRoute('/')
@@ -10,7 +11,7 @@ const handlePush = async (event) => {
   const message = JSON.parse(event.data.text())
   channel.postMessage(message)
 
-  const { title, body, timestamp } = message
+  const { title, body, timestamp, id, topic } = message
   // Possibly utilize notification tag here
   const options = {
     body,
@@ -18,9 +19,16 @@ const handlePush = async (event) => {
     badge: '/img/icons/android-chrome-any-192x192.png',
     timestamp,
     vibrate: 100,
+    data: { id, topic },
   }
   return self.registration.showNotification(title, options)
 }
+
+self.addEventListener('notificationclick', (event) => {
+  const { id, topic } = event.notification.data || {}
+  event.notification.close()
+  clients.openWindow(`${BASE_URL}/${topic}/${id}`)
+})
 
 self.addEventListener('push', (event) => {
   event.waitUntil(handlePush(event))

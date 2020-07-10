18ec1b2436e30ff120397a08b5d6ff5d7f873913
Karl Kihlström <karl.kihlstrom@softhouse.se>
2020-07-10 21:22:19 +0200

    :sparkles: Add new version update prompt

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -2,9 +2,9 @@ const channel = new BroadcastChannel('sw-push')
 
 const notificationService = {
   setupNotificationWatcher: async () => {
-    channel.addEventListener('message', () => {
+    channel.onmessage = () => {
       // To something here if the app is opemn while getting a push
-    })
+    }
   },
 }
 
